ee49ce332a1055d9cbe769ff1f78c8688926425e
Stri <stri.vip@gmail.com>
2013-10-02 12:47:03 +0800

    添加压缩方法

--- test.js
+++ test.js
@@ -1,4 +1,15 @@
 (function(module) {
   var server = require('./index');
-  server.start();
+  var fs = require('fs');
+  var path = require('path');
+  var combine = require('./lib/combine/index');
+  var dirWalk = require('./lib/util/dirWalk');
+  var task = require('./lib/util/task');
+ // server.start();
+ 	
+ 	var workSpace = '/Users/mac/Documents/sina_video_space/live_c/live_c/trunk';
+ 	var compressSpace = server.getConfig().root+'/test3'
+ 	var config = server.getConfig();
+
+ 	task.compress(workSpace,compressSpace,config);
 })(module)
