5fb82a8896463c3bebdc1af5a06e86f56db17108
Stri <stri.vip@gmail.com>
2014-01-21 15:08:18 +0800

    添加demo

---
+++ demo/addComment.js
@@ -0,0 +1,24 @@
+module.exports = function(projs){
+	var combine = projs.getCombineObject();
+
+
+	function addComment(Opts){
+		if (Opts.file.type == 'js'){
+		return '// 这里JS注释\n';
+		}
+
+		if (Opts.file.type == 'css'){
+		  return '// 这里CSS注释\n';
+		}
+	};
+
+	projs.getCombineObject = function(Opts,callback){
+		return combine(Opts,function(code){
+			code = addComment(Opts)+code;
+			callback && callback(code);
+			return code;
+		});
+	}
+
+	return projs;
+};
